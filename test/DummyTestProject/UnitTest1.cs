7f60f015503d978b4be50823dbb2bb5ec1ff29c8
Timothy Mothra Lee <tilee@microsoft.com>
2018-02-08 12:37:32 -0800

    dummy test method

---
+++ test/DummyTestProject/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.ApplicationInsights
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DummyTestMethod()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
