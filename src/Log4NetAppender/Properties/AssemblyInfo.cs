9ad1f5a10dd3b9aac0a5a2d33c0a9909074b72da
Lars Bruins Slot <l.bruinsslot@ictz.nl>
2018-06-25 10:44:41 +0200

    GenereteAssemblyInfo attribute is now false. Added AssemblyInfo

--- src/Log4NetAppender/Properties/AssemblyInfo.cs
+++ src/Log4NetAppender/Properties/AssemblyInfo.cs
@@ -1,1 +1,30 @@
-﻿
+﻿//-----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Microsoft.ApplicationInsights.Log4NetAppender")]
+[assembly: AssemblyDescription("")]
+
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+[assembly: AssemblyCompany("ICTZ B.V.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
